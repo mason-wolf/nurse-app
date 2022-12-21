7a6da8cb257b00cdfbed9b4a6b9f7769fe22fa50
Mason Wolf <masonhwolf@gmail.com>
2022-12-20 19:27:43 -0600

    Added leaflet to show patient locations

--- frontend/nurse-app/src/app/app-routing.module.ts
+++ frontend/nurse-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MapComponent } from './map/map.component';
 import { PatientProfileComponent } from './patient-profile/patient-profile.component';
 import { PatientsComponent } from './patients/patients.component';
 import { VisitsComponent } from './visits/visits.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'schedule', component: DashboardComponent },
   { path : 'patient-profile/:patient_id', component: PatientProfileComponent},
   { path: 'patients', component: PatientsComponent},
-  { path: 'visits', component: VisitsComponent}
+  { path: 'visits', component: VisitsComponent},
+  { path: 'map', component: MapComponent}
 ];
 
 @NgModule({
--- frontend/nurse-app/src/app/map/map.component.ts
+++ frontend/nurse-app/src/app/map/map.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { VisitService } from '../services/visit.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  private map;
+  constructor(private visitService: VisitService) { }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 33.20097493343204, -87.54411968949736 ],
+      zoom: 13
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    this.visitService.getWeek().subscribe(value => {
+      console.log(value);
+    });
+    
+    var marker = L.marker([33.20097493343204, -87.54411968949736]).addTo(this.map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    this.map.flyTo([33.20097493343204, -87.54411968949736], 13);
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
